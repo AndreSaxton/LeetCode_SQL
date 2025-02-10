933bcd8082509bb852a0b6d793d39c1bb6a2c3b9
AndreSaxton <andre_saxton@hotmail.com>
2025-02-09 21:14:59 -0300

    Time: 556 ms (50.30%) | Memory: 0B (100.00%) - LeetSync

--- 3711-first-letter-capitalization-ii/first-letter-capitalization-ii.sql
+++ 3711-first-letter-capitalization-ii/first-letter-capitalization-ii.sql
@@ -1,21 +1,15 @@
-/* Write your T-SQL query statement below */
-SELECT
+SELECT uc.content_id   AS content_id,
+       uc.content_text AS original_text,
+       STRING_AGG(
+         CASE WHEN IIF(CHARINDEX('-',split.value) > 0, CHARINDEX('-',split.value) ,0) > 0
+              THEN CONCAT(UPPER(SUBSTRING(split.value,1,1))
+                  ,SUBSTRING(REPLACE(split.value,RIGHT(LEFT(split.value,CHARINDEX('-',split.value)+1),2),UPPER(RIGHT(LEFT(split.value,CHARINDEX('-',split.value)+1),2))),2,LEN(split.value))
+                   )
+              ELSE CONCAT(UPPER(LEFT(split.value,1)),SUBSTRING(split.value,2,LEN(split.value)))
+         END
+       , ' ') AS converted_text
 
-uc.content_id   AS content_id,
-uc.content_text AS original_text,
-
-
-STRING_AGG(
-CASE WHEN IIF(CHARINDEX('-',split.value) > 0, CHARINDEX('-',split.value) ,0) > 0
-     THEN CONCAT(UPPER(SUBSTRING(split.value,1,1))
-      ,SUBSTRING(REPLACE(split.value,RIGHT(LEFT(split.value,CHARINDEX('-',split.value)+1),2),UPPER(RIGHT(LEFT(split.value,CHARINDEX('-',split.value)+1),2))),2,LEN(split.value))
-     )
-     ELSE CONCAT(UPPER(LEFT(split.value,1)),SUBSTRING(split.value,2,LEN(split.value)))
-END
-, ' ') AS converted_text
-
-FROM user_content uc
-CROSS APPLY STRING_SPLIT(LOWER(uc.content_text),' ') split
-
-GROUP BY uc.content_text, uc.content_id
+  FROM user_content uc
+ CROSS APPLY STRING_SPLIT(LOWER(uc.content_text),' ') split
+ GROUP BY uc.content_text, uc.content_id
 
