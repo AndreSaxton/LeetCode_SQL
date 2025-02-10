1c311ae1214039774ed4f338b7aaabe94b19e2bb
AndreSaxton <andre_saxton@hotmail.com>
2025-02-09 21:34:50 -0300

    Time: 1327 ms (62.76%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,7 +1,4 @@
 select name, population, area
   from World
-  where area >= 3000000
- union
-select name, population, area
-  from World
- where population >= 25000000
+ where area >= 3000000
+    or population >= 25000000
