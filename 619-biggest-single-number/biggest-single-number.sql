114a91b133f9702cca5db334baebe43659c95880
AndreSaxton <andre_saxton@hotmail.com>
2025-02-14 19:52:01 -0300

    Time: 592 ms (70.10%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,10 @@
-SELECT max(num) AS num
-  FROM (
-         SELECT num AS num
-           FROM MyNumbers
-          GROUP BY num
-         HAVING COUNT(num) = 1
-       ) X
+SELECT max(A.num) AS num
+  FROM MyNumbers A
+ WHERE EXISTS
+       (
+         SELECT B.num AS num
+           FROM MyNumbers B
+          GROUP BY B.num
+         HAVING COUNT(B.num) = 1
+         AND B.num = A.num
+       )
