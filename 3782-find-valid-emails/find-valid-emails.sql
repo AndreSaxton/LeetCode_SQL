2e1d2ee1afdc87555df2a8edbf7293d477ed67b3
AndreSaxton <andre_saxton@hotmail.com>
2025-02-20 22:35:45 -0300

    Time: 412 ms (81.71%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,16 +1,4 @@
-SELECT *
+SELECT user_id, email
   FROM Users A
- WHERE 1=1
---    AND A.email LIKE '%.com'
---    AND A.email LIKE '%@%'
---    AND A.email LIKE '%[^a-z0-9]+@[^a-z0-9]+[.com]'
--- AND A.email LIKE '%.com$'
-
--- AND A.email LIKE '%[@]%.com'
--- AND A.email LIKE '[a-z]%[@]%.com'
-
--- AND A.email LIKE '^[a-zA-Z0-9]+@[a-zA-Z]+.com$'
--- AND A.email LIKE '[a-zA-Z0-9]'
--- AND A.email LIKE '%[@]%'
-AND A.email LIKE '[a-z0-9]%[@][a-zA-Z]%.com'
-AND A.email NOT LIKE '%[.]%[@]%'
+ WHERE A.email LIKE '[a-z0-9]%[@][a-zA-Z]%.com'
+   AND A.email NOT LIKE '%[.]%[@]%'
