0016affc1528ec1ba3432a679b1ad01b570fa7e1
AndreSaxton <andre_saxton@hotmail.com>
2025-02-24 20:32:07 -0300

    Time: 1374 ms (65.07%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,9 +1,9 @@
 SELECT A.employee_id
      , CASE WHEN LEFT(A.name,1) <> 'M'
-             AND (CONVERT(NUMERIC(5,2),A.employee_id,2) / 2) - (A.employee_id / 2) > 0 /* PAIR NUMBER */
+            --  AND (CONVERT(NUMERIC(5,2),A.employee_id,2) / 2) - (A.employee_id / 2) > 0 /* ODD NUMBER */
+             AND  A.employee_id % 2 = 1 /* ODD NUMBER */
             THEN A.salary
             ELSE 0.00
        END AS bonus
   FROM Employees A
  ORDER BY A.employee_id
- 
