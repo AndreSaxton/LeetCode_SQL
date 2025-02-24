a52208637c158bcbd8629ab29e54b803e816f97f
AndreSaxton <andre_saxton@hotmail.com>
2025-02-24 20:08:31 -0300

    Time: 697 ms (16.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,14 @@
+SELECT X.machine_id                    AS machine_id
+     , ROUND(AVG(X.processing_time),3) AS processing_time
+  FROM (
+        SELECT A.machine_id AS machine_id
+             , A.process_id AS process_id
+             , SUM(CASE WHEN A.activity_type = 'start'
+                    THEN - A.timestamp
+                    ELSE   A.timestamp
+               END)         AS processing_time
+          FROM Activity A
+         GROUP BY A.machine_id, A.process_id
+       ) X
+ GROUP BY X.machine_id
+ ORDER BY X.machine_id ASC
