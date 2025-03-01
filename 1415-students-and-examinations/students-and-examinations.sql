6fa3f0f5a5a4224b68e5c46e7a41e184ec458d80
AndreSaxton <andre_saxton@hotmail.com>
2025-03-01 01:17:13 -0300

    Time: 1344 ms (10.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,11 @@
+SELECT A.student_id        AS student_id
+     , A.student_name      AS student_name
+     , B.subject_name      AS subject_name
+     , COUNT(C.student_id) AS attended_exams
+  FROM Students A
+  JOIN Subjects B ON 1=1
+  LEFT
+  JOIN Examinations C ON C.student_id   = A.student_id
+                     AND C.subject_name = B.subject_name
+ GROUP BY A.student_id, A.student_name, B.subject_name
+ ORDER BY A.student_id, B.subject_name
