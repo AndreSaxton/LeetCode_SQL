4b668b174f866865861667ee3d4dbe390080cb35
AndreSaxton <andre_saxton@hotmail.com>
2025-03-05 19:41:30 -0300

    Time: 1304 ms (18.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,12 @@
+SELECT A.employee_id, A.department_id
+  FROM Employee A
+ WHERE A.primary_flag = 'Y'
+
+ UNION
+
+SELECT A.employee_id, A.department_id
+  FROM Employee A
+ WHERE NOT EXISTS (SELECT TOP 1 1
+                     FROM Employee X
+                    WHERE X.employee_id  = A.employee_id
+                      AND X.primary_flag = 'Y')
