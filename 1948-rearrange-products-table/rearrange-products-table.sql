e3107356a085a2c326f3b8bc93229b4b91eb6dcd
AndreSaxton <andre_saxton@hotmail.com>
2025-03-04 23:47:36 -0300

    Time: 1790 ms (5.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -0,0 +1,10 @@
+
+SELECT product_id, store, price
+  FROM (
+        SELECT A.product_id, A.store1, A.store2, A.store3
+          FROM Products A
+       ) AS Prod
+  UNPIVOT
+       (
+        price FOR store IN (store1, store2, store3)
+       ) AS UnpivotProd
